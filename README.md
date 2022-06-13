3bb4c06441a4a089c25968128c9de4f44ab4d56a
ville.ekelund@gmail.com <ville.ekelund@gmail.com>
2022-06-13 20:24:50 +0200

    .

--- README.md
+++ README.md
@@ -22,4 +22,23 @@ Remove tokens from reserves, burn my liq. at some price.
 - Tracks sqrt(Price) instead of reserves
 - Ticks are mapped to prices with the formula 1.0001¹ -> 0.01% -> 1 basis point difference in prices
 - Ticks are ordered by price, which lets up loop thorugh them when we swap
+- Something like Price = Y / X
 
+### Price & Reserves
+
+We have reserves X and Y. We have a price 
+
+'Price = Y /X'
+
+'Y = X / Price'
+
+'X = Y * Price'
+
+## Swap
+
+'dX + X = (Y - dY) / Price'
+'dY + Y = (X - dX) / Price'
+
+## Problem
+
+- No way to loop over the prices and see how much liquidity there is
--- src/Concentrated.sol
+++ src/Concentrated.sol
@@ -4,11 +4,20 @@ pragma solidity ^0.8.13;
 contract Contract {
     constructor() payable {}
 
+    /// @notice Account -> Price -> Liquidity
+    /// @dev Maps an account to amount of liquidity owned by the account
+    mapping(address = > uint) public positions;
+
+    mapping(uint => uint) public ticks;
+
     function allocate(
         address to,
         uint256 amount,
         uint256 price
-    ) public {}
+    ) public {
+        positions[to][price] += amount;
+        ticks[price] += amount;
+    }
 
     function swap(
         uint256 direction,
@@ -16,5 +25,8 @@ contract Contract {
         uint256 price
     ) public {}
 
-    function remove(uint256 amount, uint256 price) public {}
+    function remove(uint256 amount, uint256 price) public {
+        positions[msg.sender][price] -= amount;
+        ticks[price] -= amount; // This will revert if tick[price]
+    }
 }
