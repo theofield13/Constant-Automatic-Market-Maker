f716bbfff7df8f5dea22749b792e4c0f426dce6a
ville.ekelund@gmail.com <ville.ekelund@gmail.com>
2022-06-19 13:25:44 +0200

    .

--- src/Concentrated.sol
+++ src/Concentrated.sol
@@ -4,18 +4,33 @@ pragma solidity ^0.8.13;
 contract Context {
     address public user;
 
-    ///@dev Usr -> Token -> Balance
-    mapping(address => mapping(address => uint256)) public balanceOf;
-}
-
-contract Concentrated is Context {
     address public token0;
     address public token1;
 
+    ///@dev Usr -> Token -> Balance
+    mapping(address => mapping(address => uint256)) public balanceOf;
+
+    function setToken(address token0_, address token1_) public {
+        token0 = token0_;
+        token1 = token1_;
+    }
+
+    function setBalance(
+        address user,
+        address token,
+        uint256 amount
+    ) public {
+        balanceOf[user][token] = amount;
+    }
+}
+
+contract Concentrated is Context {
     constructor() payable {}
 
     /// @dev our prices, where index = where the current price in
-    uint256[5] public grid = [1, 2, 5, 9, 11];
+    uint256[5] public grid = [1, 4, 9, 16, 25];
+
+    uint256[5] public sqrtGrid = [1, 2, 3, 4, 5];
 
     uint256 public currentIndex; // starts at 0 => price = 1
 
@@ -30,46 +45,72 @@ contract Concentrated is Context {
     mapping(uint256 => uint256) public ticks;
 
     /// @dev Token 0 -> Token 1
-    function swap(uint256 amount, uint256 limitprice) public {
-        // Get the current price.
-        uint256 currentPrice = getCurrentPrice();
-
-        // Get the liquidity at current price
-        uint256 liquidity = getLiquidityAtPrices(currentPrice);
-
-        // Get the new price
-        uint256 nextPrice = getNextPrice(currentPrice);
-
-        // Get the actual swap amounts
-        (
-            uint256 actualPrice,
+    function swap(uint256 amount, uint256 limitprice)
+        public
+        returns (
+            uint256 price,
             uint256 amountIn,
             uint256 amountOut
-        ) = getSwapAmount(amount, currentPrice, nextPrice, liquidity);
+        )
+    {
+        // Get the current price.
+        uint256 currentSqrtPrice = getCurrentSqrtPrice();
+
+        // Get the liquidity at current price
+        uint256 liquidity = getLiquidityAtSqrtPrice(currentSqrtPrice);
+
+        // Get the new price
+        uint256 nextPrice = getNextSqrtPrice(currentSqrtPrice);
+
+        // Get the actual swap amounts
+        (price, amountIn, amountOut) = getSwapAmounts(
+            amount,
+            currentSqrtPrice,
+            nextPrice,
+            liquidity
+        );
 
         // Do the actual swap
 
         // set the current price to the actual price
 
-        _setPrice(actualPrice);
+        _setPrice(price);
 
         // Update the balances
         balanceOf[msg.sender][token0] -= amountIn;
         balanceOf[msg.sender][token1] += amountOut;
     }
 
+    error setPriceError();
+
+    function _setPrice(uint256 desiredPrice) public returns (bool success) {
+        unchecked {
+            uint256 gridlength = grid.length;
+            for (uint256 i; i != gridlength; ++i) {
+                uint256 priceOnGrid = grid[i];
+
+                if (desiredPrice == priceOnGrid) {
+                    currentIndex = i;
+                    success = true;
+                    break;
+                }
+            }
+        }
+        if (!success) revert setPriceError();
+    }
+
     /// @dev for now use a state variable
-    function getCurrentPrice() public view returns (uint256) {
-        return grid[currentIndex];
+    function getCurrentSqrtPrice() public view returns (uint256) {
+        return sqrtGrid[currentIndex];
     }
 
     /// @dev Lookup some data structure using currentPrice as its key and returns some liquidity amount
-    function getLiquidityAtPrices(uint256 price)
+    function getLiquidityAtSqrtPrice(uint256 price)
         public
         view
         returns (uint256 liquidity)
     {
-        return ticks[price];
+        return ticks[sqrtPrice];
     }
 
     /// @dev Lookup next higher price given a price
@@ -81,15 +122,79 @@ contract Concentrated is Context {
         nextPrice = grid[currentIndex + 1];
     }
 
-    // ---To Do ---//
+    function getSwapAmounts(
+        uint256 swapAmount,
+        uint256 currentPrice,
+        uint256 nextPrice,
+        uint256 liquidity
+    )
+        public
+        view
+        returns (
+            uint256 price,
+            uint256 amountIn,
+            uint256 amountOut
+        )
+    {
+        amountIn = swapAmount;
 
+        uint256 deltaSqrtPrice = getChangeInPriceGivenX(amountIn, liquidity);
+
+        amountOut = getChangeInYGivenPrice(deltaSqrtPrice, liquidity);
+
+        if (deltaSqrtPrice > (nextPrice - currentPrice)) price = nextPrice;
+        else price = currentPrice + deltaSqrtPrice;
+    }
+
+    /// @dev Δx = Δ(1/sqrt(Price)) * L
+    ///      Δx = Δ(1/sqrt(PriceNext)) - sqrt(PriceCurrent)) * L
+    ///      Δx / L = 1 / (√P_1 - √P_0)
+    ///      Δp = L / Δx
+    function getChangeInPriceGivenX(uint256 deltaX, uint256 liquidity)
+        public
+        view
+        returns (uint256 deltaSqrtPrice)
+    {
+        deltaSqrtPrice = (liquidity * 1e18) / deltaX; // Unit Math: 1e18 = 1e18 * 1e18 / 1e18
+    }
+
+    /// @dev Δy = Δ(sqrt(Price)) * L
+    function getChangeInYGivenPrice(uint256 deltaSqrtPrice, uint256 liquidity)
+        public
+        view
+        returns (uint256 deltaY)
+    {
+        deltaY = (deltaSqrtPrice * liquidity) / 1e18; //Unint math: 1e18 = 1e18 * 1e18 / 1e18
+    }
+
+    // ---To Do ---//
+    /// @dev L = sqrt(xy) - sqrt(k)
+    /// Price = y / x
+    /// L = sqrt(x * p * x)
+    /// L = sqrt(x² * p)
+    /// L = sqrt(p) * x
+    /// liquidity = x * sqrtPrice
+    /// x = liquidity / sqprtPrice
+    /// xy = k
+    /// L² = xy
+
+    ///liquidity = L  = liquidity we want to mint
     function allocate(
         address to,
-        uint256 amount,
-        uint256 price
-    ) public {
-        positions[msg.sender][price] += amount;
-        ticks[price] += amount;
+        uint256 liquidity,
+        uint256 sqrtPrice
+    ) public returns (uint256 amount0, uint256 amount1) {
+        // Compute token amounts required to mint liquidity at price
+        amount0 = (liquidity * 1e18) / sqrtPrice; // sqrtPrice = 1e18 units
+        amount1 = (amount1 * 1e18) / ((liquidity * liquidity) / 1e18); // 1e18 * 1e18 / (1e18 * 1e18 / 1e18) = 1e18 * 1e18 / 1e18 = 1e18  units
+
+        // set liquidity at sqrtPrice
+        ticks[sqrtPrice] = liquidity;
+
+        // handle tokens and minting liquidity
+        balanceOf[to][token0] -= amount0;
+        balanceOf[to][token1] -= amount1;
+        balanceOf[to][address(this)] += liquidity;
     }
 
     function remove(uint256 amount, uint256 price) public {
--- test/Concentrated.t.sol
+++ test/Concentrated.t.sol
@@ -7,13 +7,55 @@ import "../src/Concentrated.sol";
 contract TestConcentrated is Test {
     Concentrated public concentrate;
 
+    address public user;
+    address public token0;
+    address public token1;
+
     function setUp() public {
         concentrate = new Concentrated();
         vm.prank(address(0));
         vm.deal(address(0), 100 ether);
+
+        user = address(0x01);
+        token0 = address(0x04);
+        token0 = address(0x05);
+
+        concentrate.setToken(token0, token1);
+        concentrate.setBalance(user, token0, 100 ether);
+    }
+
+    uint256 public constant SCALAR = 1e18;
+
+    function allocate() public returns (uint256 amount0, uint256 amount1) {
+        vm.prank(user);
+        uint256 liquidity = 1 * SCALAR;
+        uint256 priceIndex = 1;
+        uint256 sqrtPrice = concentrate.sqrtGrid(priceIndex) * SCALAR;
+        (amount0, amount1) = concentrate.allocate(user, liquidity, sqrtPrice);
+
+        assertEq(concentrate.balanceOf(user, address(concentrate)), liquidity);
+    }
+
+    function testAllocate() public {
+        (uint256 amount0, uint256 amount1) = allocate();
+        console.log("Allocated:");
+        console.log(amount0);
+        console.log(amount1);
     }
 
     function testSwap() public {
-        concentrate.swap(1, 1);
+        vm.prank(user);
+        uint256 amount = 750;
+        uint256 limitPrice = 0;
+
+        (uint256 price, uint256 amountIn, uint256 amountOut) = concentrate.swap(
+            amount,
+            limitPrice
+        );
+        console.log(price);
+        console.log(amountIn);
+        console.log(amountOut);
+
+        assertGt(concentrate.balanceOf(user, token1), 0);
     }
 }
